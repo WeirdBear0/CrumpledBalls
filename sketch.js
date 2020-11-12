d030a3fb54b8a8320305656df3f5e5175ee37a9a
WeirdBear0 <66538986+WeirdBear0@users.noreply.github.com>
2020-11-11 18:03:49 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -46,7 +46,7 @@ function draw() {
 
 function keyPressed(){
 	if(keyCode === UP_ARROW){
-		Matter.Body.applyForce(paper.body, paper.body.position,{x:1000, y:540});
+		Matter.Body.applyForce(paper.body, paper.body.position,{x:85, y:-85});
 		console.log("eafgsafa");
 	}
 }
